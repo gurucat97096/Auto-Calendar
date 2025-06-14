f64178db4502f774f2011b130ab935e6a3b43a4b
gurucat97096 <ryanloe1@gmail.com>
2025-06-14 12:14:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import os
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-DB_PATH = "events.db"
-conn = sqlite3.connect(DB_PATH)
-cursor = conn.cursor()
+DB_PATH  = "events.db"
+conn     =  sqlite3.connect(DB_PATH)
+cursor   =  conn.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
